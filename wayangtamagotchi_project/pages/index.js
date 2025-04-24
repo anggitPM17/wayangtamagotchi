2b81ba4b02be51ca9e2b2adcb665dee3e255db8e
anggitPM17 <167511000+anggitPM17@users.noreply.github.com>
2025-04-24 19:36:36 +0700

    Create index.js

---
+++ wayangtamagotchi_project/pages/index.js
@@ -0,0 +1,13 @@
+// pages/index.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Selamat Datang di Wayang Tamagotchi!</h1>
+      <p>Ini adalah halaman utama proyek kamu.</p>
+    </div>
+  );
+}
+
+export default Home;
